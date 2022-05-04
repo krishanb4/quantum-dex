6580f5c114c177c411a27f3ba62c9260b969e8ab
krishanb4 <krishanbandara4444@gmail.com>
2022-05-04 22:39:02 +0530

    sentry

--- next.config.js
+++ next.config.js
@@ -101,4 +101,4 @@ const config = {
   },
 }
 
-module.exports = withBundleAnalyzer(withSentryConfig(config))
+module.exports = withBundleAnalyzer(withSentryConfig(config, sentryWebpackPluginOptions))
